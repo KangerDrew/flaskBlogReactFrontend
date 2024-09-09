e72fe177c16f37bc172458a5f388fca9e37bcb71
Drew Kang <kangdrew95@gmail.com>
2024-09-09 10:29:25 -0700

    default error 404 route added

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
+import NotFound from "./pages/NotFound"
 
 const Router = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<LandingPage/>} />
+            <Route path="*" element={<NotFound/>}/>
         </Routes>
     </BrowserRouter>
   )
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,6 +4,10 @@ const LandingPage: React.FC = () => {
   return (
     <div>
         <h1>Welcome to the Blog</h1>
+        <h3>You are not that guy</h3>
+        <a href="/login">
+            <button>Login</button>
+        </a>
     </div>
   )
 }
