0a0679e8f49821cdd2bd5444a87ff927ea3350ea
Drew Kang <kangdrew95@gmail.com>
2024-09-11 14:52:38 -0700

    simple login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,11 +2,36 @@ import React, { useState } from 'react'
 
 export const LoginPage: React.FC = () => {
 
-	const [email, setEmail] = useState<String | null>(null);
-	const [password, setPassword] = useState<String | null>(null);
+	const [userID, setUserID] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+
+	const loginUser = () => {
+		console.log(userID, password);
+	}
 
   	return (
-    <div>LoginPage :D</div>
+    <div>
+		<h1>Provide your Login Info Here:</h1>
+		<form action="">
+			<div>
+				<label>User ID</label>
+				<input
+					type="text"
+					value={userID}
+					onChange={(e) => setUserID(e.target.value)}
+				/>
+			</div>
+			<div>
+				<label>Password</label>
+				<input
+					type="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+			</div>
+		<button type="button" onClick={() => loginUser()}>Submit</button>
+		</form>
+	</div>
   	)
 }
 
